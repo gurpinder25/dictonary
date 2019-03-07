00085d3980be885c34d9f127f92609966fa2312b
HP <HP@LAPTOP-UNJTSKGF>
2019-03-07 14:00:57 +1300

    Add project files.

---
+++ dictonary/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace dictonary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.txtbox = new System.Windows.Forms.TextBox();
+            this.lbx = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(210, 131);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 0;
+            this.btn2.Text = "clear";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(291, 131);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(87, 60);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "charactercalculation";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // txtbox
+            // 
+            this.txtbox.Location = new System.Drawing.Point(210, 88);
+            this.txtbox.Name = "txtbox";
+            this.txtbox.Size = new System.Drawing.Size(181, 20);
+            this.txtbox.TabIndex = 2;
+            this.txtbox.TextChanged += new System.EventHandler(this.txtbox_TextChanged);
+            // 
+            // lbx
+            // 
+            this.lbx.FormattingEnabled = true;
+            this.lbx.Location = new System.Drawing.Point(412, 88);
+            this.lbx.Name = "lbx";
+            this.lbx.Size = new System.Drawing.Size(154, 186);
+            this.lbx.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbx);
+            this.Controls.Add(this.txtbox);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.TextBox txtbox;
+        private System.Windows.Forms.ListBox lbx;
+    }
+}
+
---
+++ dictonary/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dictonary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            var Letters = new Dictionary<char, int>();
+
+             string word = File.ReadAllText("Goppi.txt");
+                //"if word is the word and bird is the wordthen word is the bird";
+            // for each letter in the word
+            foreach (char a in word)
+            {
+//if there is no entry in the dictionary then add a new entry with 1as the counter
+                if (Letters.ContainsKey(a) == false)
+                {
+                    Letters.Add(a, 1);
+                }
+                else
+                    // if its already in the dictionary increase the number (value) by one.
+                {
+                    //finds the key and adds 1 to the value, basically counts up
+                    Letters[a]++;
+                }
+            }
+            //loop through the dictionary and get out the key/value pairs
+
+            foreach (KeyValuePair<char, int> pair in Letters)
+            {
+                lbx.Items.Add(pair.Key + " --> " + pair.Value);
+            }
+        }
+    }
+}
